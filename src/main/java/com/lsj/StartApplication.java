cb3d5f34902510486409461686601b26b92fcbb9
lushunjian <771243321@qq.com>
2018-10-23 15:42:37 +0800

    树状联网模型

---
+++ src/main/java/com/lsj/StartApplication.java
@@ -0,0 +1,19 @@
+package com.lsj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Auther: Lushunjian
+ * @Date: 2018/8/21 22:55
+ * @Description:
+ */
+@SpringBootApplication
+public class StartApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(StartApplication.class, args);
+
+    }
+}
